2ed10bc5d982835ba5ecba646dd1a9015e7173d7
abtinf <abtinf@gmail.com>
2024-02-28 13:37:40 -0800

    Do not exit on failure to init embedded postgres

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"log/slog"
 	"os"
 	"os/signal"
@@ -37,26 +36,27 @@ func run(ctx context.Context, log *slog.Logger, lookupenv func(string) (string, 
 			Logger(initlogs).
 			CachePath(filepath.Join(os.TempDir(), "embedded-postgres")))
 		if err := pg.Start(); err != nil {
-			return fmt.Errorf("failed to start embedded postgres: %w", err)
-		}
-		log.Info("embedded postgres started", "logs", initlogs.String())
-		initlogs.Reset()
-		go func() {
-			t := time.Tick(time.Duration(c.MonitorInterval) * time.Second)
-			for range t {
-				l := initlogs.String()
-				if l != "" {
-					log.Info("embedded postgres logs", "logs", l)
-					initlogs.Reset()
+			log.Error("failed to started embedded postgres; server may already be running", "error", err)
+		} else {
+			log.Info("embedded postgres started", "logs", initlogs.String())
+			initlogs.Reset()
+			go func() {
+				t := time.Tick(time.Duration(c.MonitorInterval) * time.Second)
+				for range t {
+					l := initlogs.String()
+					if l != "" {
+						log.Info("embedded postgres logs", "logs", l)
+						initlogs.Reset()
+					}
 				}
-			}
-		}()
-		defer func() {
-			if err := pg.Stop(); err != nil {
-				log.Error("failed to stop embedded postgres", "error", err)
-			}
-			log.Info("embedded postgres stopped", "logs", initlogs.String())
-		}()
+			}()
+			defer func() {
+				if err := pg.Stop(); err != nil {
+					log.Error("failed to stop embedded postgres", "error", err)
+				}
+				log.Info("embedded postgres stopped", "logs", initlogs.String())
+			}()
+		}
 	}
 
 	srv, err := server.New(ctx, log, c)
